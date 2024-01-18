3e290764273e0aa83f18b47c9e4036c12eba2181
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-01-18 11:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ get species()
 	return this._species;
 }
 makeSound(){
-	console.log(`The ${this._species} makes a sound`);
+	console.log(`The ${species} makes a sound`);
 }
 class Dog extends Animal {
 	bark(){
